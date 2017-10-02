908d08f47866724e269d2416e18932c671a571a5
David Prause <praused@hotmail.com>
2017-10-02 15:20:35 -0400

    Segment 2, Demo 1, Extending State

---
+++ Sixeyed.Extensions.Advanced.Tests/Demo1/InstrumentationTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sixeyed.Extensions.Advanced.Demo1;
+using System.Threading;
+
+namespace Sixeyed.Extensions.Advanced.Tests.Demo1
+{
+    [TestClass]
+    public class InstrumentationTests
+    {
+        [TestMethod]
+        public void GetTotalSeconds()
+        {
+            var instrumentation = new Instrumentation();
+            instrumentation.Start();
+            Thread.Sleep(750);
+            Assert.AreEqual(1, instrumentation.GetElapsedTime()); //passes bacause GetElapsedTime() rounds to whole seconds
+        }
+
+        [TestMethod]
+        public void GetPreciseElapsedTime()
+        {
+            var instrumentation = new Instrumentation();
+            instrumentation.Start();
+            Thread.Sleep(750);
+            var elapsed = instrumentation.GetPreciseElapsedTime();
+            //Assert.IsTrue(elapsed >= 0.75 && elapsed < 0.751); //fails because using reflection (in GetPreciseElapsedTime) takes time.
+            Assert.IsTrue(elapsed >= 0.75 && elapsed < 0.78);//passes because we broadened the range.
+            //Could have fixed by taking the ending time before using reflection istead, but this example shows the limitations of reflection. 
+        }
+
+        [TestMethod]
+        public void GetReallyPreciseElapsedTime()
+        {
+            var instrumentation = new Instrumentation();
+            instrumentation.StartWithPrecision();
+            Thread.Sleep(750);
+            Assert.AreEqual(750, instrumentation.GetReallyPreciseElapsedTime());
+        }
+    }
+}
---
+++ Sixeyed.Extensions.Advanced/Demo1/InstrumentationExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Sixeyed.Extensions.Advanced.Demo1
+{
+    public static  class InstrumentationExtensions
+    {
+        //since the Instrumentation class has a unique id, we can simulate its state here using this static collection.
+        private static Dictionary<Guid, Stopwatch> _Stopwatches = new Dictionary<Guid, Stopwatch>();
+        //There's no garbage collection for this extended state, so it will just grow continually. That's a concern.
+
+        public static double GetPreciseElapsedTime(this Instrumentation instrumentation)
+        {
+            //var startedAt = instrumentation._startedAt; //can't do this because _startedAt is a private field.
+            //try workaround using reflection...
+            var fieldInfo = typeof(Instrumentation).GetField("_startedAt", BindingFlags.Instance | BindingFlags.NonPublic);
+            var startedAt = (DateTime)fieldInfo.GetValue(instrumentation);
+            return new TimeSpan(DateTime.Now.Ticks - startedAt.Ticks).TotalSeconds;
+        }
+
+        //We'll use this method instead of the original start method (on the Instrumentation class)
+        public static void StartWithPrecision(this Instrumentation instrumentation)
+        {
+            _Stopwatches[instrumentation.Id] = Stopwatch.StartNew();
+        }
+
+        public static long GetReallyPreciseElapsedTime(this Instrumentation instrumentation)
+        {
+            return _Stopwatches[instrumentation.Id].ElapsedMilliseconds;
+        }
+    }
+}
