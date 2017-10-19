63d189079ab1513fb7351db19c954e8dcb57a0ad
David Prause <praused@hotmail.com>
2017-10-19 11:41:36 -0400

    Segment 3, Demo 5, CompilerBinding

---
+++ Sixeyed.Extensions.Advanced.Tests/Demo5/DecimalExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sixeyed.Extensions.Advanced.Demo5;
+
+namespace Sixeyed.Extensions.Advanced.Tests.Demo5
+{
+    [TestClass]
+    public class DecimalExtensionsTests
+    {
+        [TestMethod]
+        public void ToString()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10.5", input.ToString()); // fails because the class implementation of ToString is called
+        }
+
+        [TestMethod]
+        public void ToStringRounded()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10.5", input.ToStringRounded()); // passes, runs most specific extension
+        }
+
+        [TestMethod]
+        public void ToStringRoundedIComparable()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10.5", ((IComparable)input).ToStringRounded()); // fails because of cast
+        }
+
+        [TestMethod]
+        public void ToStringRoundedIComparableOfDecimal()
+        {
+            var input = 10.51M;
+            Assert.AreEqual("10.5", ((IComparable<decimal>)input).ToStringRounded()); // fails because of cast
+        }
+    }
+}
---
+++ Sixeyed.Extensions.Advanced/Demo5/DecimalExtensions2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Extensions
+// disambiguate extension method with the same signature and priority by changing the namespace
+{
+    public static class DecimalExtensions2
+    {        
+        public static string ToStringRounded(this decimal input)
+        {
+            return Math.Round(input, 2).ToString();
+        }
+    }
+}
