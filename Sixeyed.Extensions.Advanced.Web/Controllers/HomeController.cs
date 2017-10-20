a930a6ab62735dc4f4603dc97f0bdc0f75aba9d9
David Prause <praused@hotmail.com>
2017-10-20 16:06:27 -0400

    Segment 3, Demo 6, Configuration Binding

---
+++ Sixeyed.Extensions.Advanced.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Recaptcha;
+
+namespace Sixeyed.Extensions.Advanced.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [RecaptchaControlMvc.CaptchaValidator]
+        public ActionResult Submit(bool captchaValid)
+        {
+            if (!captchaValid)
+            {
+                ModelState.AddModelError("", "reCAPTCHA invalid");
+            }
+            if (ModelState.IsValid)
+            {
+                return View("ThankYou");
+            }
+            return View("Index");           
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Sixeyed.Extensions.Advanced.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sixeyed.Extensions.Advanced.Web.Startup))]
+namespace Sixeyed.Extensions.Advanced.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
